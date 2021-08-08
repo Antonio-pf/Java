068f1128b32fee3f316d68941e07094c601942d6
Antonio-pf <antoniopfelipe03@gmail.com>
2021-08-08 13:40:47 -0300

    Corrido identação

--- Exercicios/src/exer02.java
+++ Exercicios/src/exer02.java
@@ -9,15 +9,20 @@ public class exer02 {
 
         System.out.println("Digite seu usuário:");
         nome = scan.next();
+
         System.out.println("Digite sua senha:");
         senha = scan.next();
 
+
         do {
             System.out.println("Usuário e senha não podem ser iguais!");
+
             System.out.println("Usuário: ");
             nome = scan.next();
+
             System.out.println("Senha: ");
             senha = scan.next();
+
         }while (nome.equals(senha) == true);
     }
 }
