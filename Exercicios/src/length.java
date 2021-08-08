908964652ec26978334c73c41b07d9133cdef4a1
Antonio-pf <antoniopfelipe03@gmail.com>
2021-08-08 15:44:16 -0300

    review tipos de length

---
+++ Exercicios/src/length.java
@@ -0,0 +1,14 @@
+public class length {
+
+    public static void main(String[] args) {
+        //testando .length
+        //*no array
+        int [] array = new int[4];
+        System.out.println("Tamanho do array é: " + array.length);
+
+        //em string
+        String test = "Hello baby";
+        System.out.println("Tamanho da String: " + test.length());
+
+    }
+}
